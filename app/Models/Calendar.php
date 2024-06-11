b750712c0a33f7de0de49d459a8146d00e99302d
jorgecastano <jcastanor_ext@obtenmas.com>
2024-06-11 15:58:25 -0500

    Add new resources

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Calendar extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'year',
+        'state',
+    ];
 }
--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Timesheet extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'calendar_id',
+        'type',
+        'day_in',
+        'day_out',
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
