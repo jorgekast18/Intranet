5d51a9beb8e33c451151796d31ca34cbd7505f19
Jorge Castano <jacr011893@gmail.com>
2024-06-11 20:55:50 -0500

    Create new resource holidat

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -8,13 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Holiday extends Model
 {
     use HasFactory;
-    public function user()
-    {
+    protected $guarded = [];
+    public function user(){
         return $this->belongsTo(User::class);
     }
-
-    public function calendar()
-    {
+    public function calendar(){
         return $this->belongsTo(Calendar::class);
     }
 }
